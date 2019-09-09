c5acaf1eb202804af7413255b680f4a95166ec8e
Pat Palmerston <patpalmerston@gmail.com>
2019-09-09 10:33:07 -0700

    using knex to retrieve data

--- posts/post-router.js
+++ posts/post-router.js
@@ -6,23 +6,24 @@ const db = require('../data/db-config.js');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-
+  // get data from database and return it to client
+  // select * from posts
+	db.select('*')
+		.from('posts')
+		.then(posts => {
+			res.status(200).json(posts);
+		})
+		.catch(err => {
+			res.json(err);
+		});
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res) => {});
 
-});
+router.post('/', (req, res) => {});
 
-router.post('/', (req, res) => {
+router.put('/:id', (req, res) => {});
 
-});
-
-router.put('/:id', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
+router.delete('/:id', (req, res) => {});
 
 module.exports = router;
